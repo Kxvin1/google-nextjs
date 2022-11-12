941d36b49f1b2e2d93268b7f3ad507ed2947b4c5
kevin <kevinzy17@gmail.com>
2022-11-12 12:06:12 -0800

    added authentication with google cloud via nextauth and styled

--- components/User.js
+++ components/User.js
@@ -1,5 +1,34 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
+
 function User() {
-  return <div>User</div>;
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        <Image
+          src={session.user.image}
+          alt="user-img"
+          height={50}
+          width={50}
+          className="rounded-full cursor-pointer hover:bg-gray-200 p-1"
+          onClick={signOut}
+        />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <button
+          onClick={signIn}
+          className="hover:underline bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md"
+        >
+          Sign In
+        </button>
+      </>
+    );
+  }
 }
 
 export default User;
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,24 +1,32 @@
 import React from "react";
 import Header from "../../components/Header";
-import { getProviders } from "next-auth/react";
+import { getProviders, signIn } from "next-auth/react";
 import Image from "next/image";
 
 // client side
-export default function signIn({ providers }) {
+export default function signin({ providers }) {
   return (
     <>
       <Header />
-      <div>
+      <div className="mt-40">
         {Object.values(providers).map((provider) => (
-          <div key={provider.name}>
+          <div key={provider.name} className="flex flex-col items-center">
             <Image
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1280px-Google_2015_logo.svg.png"
               alt="goog"
               width={300}
               height={100}
+              className="w-52 object-cover"
             />
-            <p>Google Next.js Clone</p>
-            <button>Sign in with {provider.name}</button>
+            <p className="text-sm italtic my-10 text-center">
+              Google Next.js Clone
+            </p>
+            <button
+              className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              Sign in with {provider.name}
+            </button>
           </div>
         ))}
       </div>
