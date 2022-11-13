1d5fc48c93196fe447b5cc30eb2f6394dc791823
kevin <kevinzy17@gmail.com>
2022-11-13 12:57:35 -0800

    implemented google programming search engine api

---
+++ Response.js
@@ -0,0 +1,453 @@
+export default {
+  kind: "customsearch#search",
+  url: {
+    type: "application/json",
+    template:
+      "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json",
+  },
+  queries: {
+    request: [
+      {
+        title: "Google Custom Search - hi",
+        totalResults: "10690000000",
+        searchTerms: "hi",
+        count: 10,
+        startIndex: 1,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "64a1a23fcaf8b4191",
+      },
+    ],
+    nextPage: [
+      {
+        title: "Google Custom Search - hi",
+        totalResults: "10690000000",
+        searchTerms: "hi",
+        count: 10,
+        startIndex: 11,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "64a1a23fcaf8b4191",
+      },
+    ],
+  },
+  context: {
+    title: "go-nextjs",
+  },
+  searchInformation: {
+    searchTime: 0.32778,
+    formattedSearchTime: "0.33",
+    totalResults: "10690000000",
+    formattedTotalResults: "10,690,000,000",
+  },
+  items: [
+    {
+      kind: "customsearch#result",
+      title: "Web3 Neobank | Trade, Save & Spend Crypto & Fiat | hi",
+      htmlTitle:
+        "Web3 Neobank | Trade, Save &amp; Spend Crypto &amp; Fiat | <b>hi</b>",
+      link: "https://hi.com/",
+      displayLink: "hi.com",
+      snippet:
+        "Buy, Trade, Send and Earn Crypto & Fiat. Deposit Bitcoin, ETH, USDT and other cryptos and start earning. Get the hi Debit Card and Multi-Currency IBAN ...",
+      htmlSnippet:
+        "Buy, Trade, Send and Earn Crypto &amp; Fiat. Deposit Bitcoin, ETH, USDT and other cryptos and start earning. Get the <b>hi</b> Debit Card and Multi-Currency IBAN&nbsp;...",
+      cacheId: "Npa3zwBnjj0J",
+      formattedUrl: "https://hi.com/",
+      htmlFormattedUrl: "https://<b>hi</b>.com/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTH0arVX55vEvqwIN4Uv_ICk7IHJa0SRGZeNaG9-jfcSBtT7SqOZ566meI9",
+            width: "284",
+            height: "177",
+          },
+        ],
+        metatags: [
+          {
+            "og:image": "https://www.hi.com/images/websitePreview/Landing.png",
+            "og:type": "website",
+            "twitter:card": "summary_large_image",
+            "twitter:title":
+              "Web3 Neobank | Trade, Save & Spend Crypto & Fiat | hi",
+            "og:site_name": "hi",
+            "og:title": "Web3 Neobank | Trade, Save & Spend Crypto & Fiat | hi",
+            "og:description":
+              "Buy, Trade, Send and Earn Crypto & Fiat. Deposit Bitcoin, ETH, USDT and other cryptos and start earning. Get the hi Debit Card and Multi-Currency IBAN Account.",
+            "twitter:image":
+              "https://www.hi.com/images/websitePreview/Landing.png",
+            "next-head-count": "33",
+            "twitter:site": "@hi_com_official",
+            viewport: "width=device-width, initial-scale=1, user-scalable=no",
+            "twitter:description":
+              "Buy, Trade, Send and Earn Crypto & Fiat. Deposit Bitcoin, ETH, USDT and other cryptos and start earning. Get the hi Debit Card and Multi-Currency IBAN Account.",
+            "og:url": "https://hi.com",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://hi.com/images/landing/ezgif-4-d1d76fab0d.gif",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Hawaii.gov | The Official Website of the Aloha State",
+      htmlTitle: "<b>Hawaii</b>.gov | The Official Website of the Aloha State",
+      link: "https://portal.ehawaii.gov/",
+      displayLink: "portal.ehawaii.gov",
+      snippet:
+        "Aloha, welcome to hawaii.gov The official website of the Aloha State · Latest News · Governor Ige · Viewing Marine Wildlife in Hawaiʻi Responsibly · Weather.",
+      htmlSnippet:
+        "Aloha, welcome to <b>hawaii</b>.gov The official website of the Aloha State &middot; Latest News &middot; Governor Ige &middot; Viewing Marine Wildlife in Hawaiʻi Responsibly &middot; Weather.",
+      cacheId: "zEBKJyFMA88J",
+      formattedUrl: "https://portal.ehawaii.gov/",
+      htmlFormattedUrl: "https://portal.e<b>hawaii</b>.gov/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZx6YcXPYDhZnXPqGEGR5BkEX4kVHdhysqPK_v9eSJ3_nhQPvcDHI2AmHx",
+            width: "225",
+            height: "225",
+          },
+        ],
+        metatags: [
+          {
+            "theme-color": "#207fa1",
+            "og:site_name": "Hawaii.gov",
+            viewport: "width=device-width,initial-scale=1",
+            "og:title": "Hawaii.gov",
+            "og:url": "https://portal.ehawaii.gov",
+            "og:description":
+              "Find resources for Government, Residents, Business and Visitors on Hawaii.gov.",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://pbs.twimg.com/media/FJUN4aDXwAEibTI?format=jpg&name=small",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Hi Definition & Meaning - Merriam-Webster",
+      htmlTitle: "<b>Hi</b> Definition &amp; Meaning - Merriam-Webster",
+      link: "https://www.merriam-webster.com/dictionary/hi",
+      displayLink: "www.merriam-webster.com",
+      snippet:
+        "Nov 1, 2022 ... The meaning of HI is —used especially as a greeting. How to use hi in a sentence.",
+      htmlSnippet:
+        "Nov 1, 2022 <b>...</b> The meaning of <b>HI</b> is —used especially as a greeting. How to use <b>hi</b> in a sentence.",
+      cacheId: "7Mq6x4g737UJ",
+      formattedUrl: "https://www.merriam-webster.com/dictionary/hi",
+      htmlFormattedUrl: "https://www.merriam-webster.com/dictionary/<b>hi</b>",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSKksTSKYwpW8It403nrjw5t1_a8pLO2PI6ImEG7uvFrNfacgiziPZgG-_O",
+            width: "225",
+            height: "225",
+          },
+        ],
+        metatags: [
+          {
+            "msapplication-tilecolor": "#2b5797",
+            "og:image":
+              "https://merriam-webster.com/assets/mw/static/social-media-share/mw-logo-245x245@1x.png",
+            "twitter:title": "Definition of HI",
+            "twitter:card": "summary",
+            "theme-color": "#ffffff",
+            "twitter:url": "https://www.merriam-webster.com/dictionary/hi",
+            "og:title": "Definition of HI",
+            "twitter:aria-text": "Share the Definition of hi on Twitter",
+            "og:aria-text": "Post the Definition of hi to Facebook",
+            "og:description":
+              "—used especially as a greeting… See the full definition",
+            "twitter:image":
+              "https://merriam-webster.com/assets/mw/static/social-media-share/mw-logo-245x245@1x.png",
+            referrer: "unsafe-url",
+            "fb:app_id": "178450008855735",
+            "twitter:site": "@MerriamWebster",
+            viewport: "width=device-width, initial-scale=1.0",
+            "twitter:description":
+              "—used especially as a greeting… See the full definition",
+            "og:url": "https://www.merriam-webster.com/dictionary/hi",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://merriam-webster.com/assets/mw/static/social-media-share/mw-logo-245x245@1x.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Government",
+      htmlTitle: "Government",
+      link: "https://www.honolulu.gov/",
+      displayLink: "www.honolulu.gov",
+      snippet:
+        "Sep 28, 2020 ... Original text · America's Best-in-State Employers Hawaii Forbes 2022 · Career Fair Banner · CITY COVID-19 INFORMATION · Driver License & State ID ...",
+      htmlSnippet:
+        "Sep 28, 2020 <b>...</b> Original text &middot; America&#39;s Best-in-State Employers <b>Hawaii</b> Forbes 2022 &middot; Career Fair Banner &middot; CITY COVID-19 INFORMATION &middot; Driver License &amp; State ID&nbsp;...",
+      cacheId: "K0YioazRrZ0J",
+      formattedUrl: "https://www.honolulu.gov/",
+      htmlFormattedUrl: "https://www.honolulu.gov/",
+      pagemap: {
+        metatags: [
+          {
+            "twitter:title": "blank",
+            viewport: "width=device-width, initial-scale=1.0",
+            "twitter:description": "&nbsp;\r&nbsp;\r&nbsp;",
+            "og:title": "blank",
+            "og:description": "&nbsp;\r&nbsp;\r&nbsp;",
+          },
+        ],
+        article: [
+          {
+            inlanguage: "en-GB",
+            datemodified: "2020-09-28T13:13:27-10:00",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "hi - Wiktionary",
+      htmlTitle: "<b>hi</b> - Wiktionary",
+      link: "https://en.wiktionary.org/wiki/hi",
+      displayLink: "en.wiktionary.org",
+      snippet:
+        "A friendly, informal, casual greeting said upon someone's arrival. quotations ▽synonyms △. Synonyms: hello, greetings, howdy. Hi, how are you? I just dropped ...",
+      htmlSnippet:
+        "A friendly, informal, casual greeting said upon someone&#39;s arrival. quotations ▽synonyms △. Synonyms: hello, greetings, howdy. <b>Hi</b>, how are you? I just dropped&nbsp;...",
+      cacheId: "K0YmYzI7FksJ",
+      formattedUrl: "https://en.wiktionary.org/wiki/hi",
+      htmlFormattedUrl: "https://en.wiktionary.org/wiki/<b>hi</b>",
+      pagemap: {
+        metatags: [
+          {
+            referrer: "origin",
+            "theme-color": "#eaecf0",
+            "og:type": "website",
+            viewport:
+              "width=device-width, initial-scale=1.0, user-scalable=yes, minimum-scale=0.25, maximum-scale=5.0",
+            "og:title": "hi - Wiktionary",
+            "format-detection": "telephone=no",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Hi Definition & Meaning | Dictionary.com",
+      htmlTitle: "<b>Hi</b> Definition &amp; Meaning | Dictionary.com",
+      link: "https://www.dictionary.com/browse/hi",
+      displayLink: "www.dictionary.com",
+      snippet:
+        "Hi definition, (used as an exclamation of greeting); hello! See more.",
+      htmlSnippet:
+        "<b>Hi</b> definition, (used as an exclamation of greeting); hello! See more.",
+      cacheId: "WhCXY6ZHEtsJ",
+      formattedUrl: "https://www.dictionary.com/browse/hi",
+      htmlFormattedUrl: "https://www.dictionary.com/browse/<b>hi</b>",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSwcf3SjW2-7T0Y8Gq-s5mGt58UG4-m-JDLMIcHlGTPisKRDoIb3EwhmeC7",
+            width: "225",
+            height: "225",
+          },
+        ],
+        metatags: [
+          {
+            "og:image":
+              "https://www.dictionary.com/assets/dictionary-social-logo-93d290770ddf5b8c309b6e5ce9dcaac9.png",
+            "twitter:card": "summary",
+            "fb:app_id": "127444090629600",
+            "og:site_name": "www.dictionary.com",
+            "twitter:site": "@Dictionarycom",
+            "msvalidate.01": "DF5542D7723770377E9ABFF59AC1DC97",
+            viewport: "width=device-width, initial-scale=1.0",
+            "og:title": "Definition of hi | Dictionary.com",
+            "fb:admins": "100000304287730;109125464873",
+            "og:description":
+              "Hi definition, (used as an exclamation of greeting); hello! See more.",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://www.dictionary.com/assets/dictionary-social-logo-93d290770ddf5b8c309b6e5ce9dcaac9.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Hawaii Judiciary",
+      htmlTitle: "<b>Hawaii</b> Judiciary",
+      link: "https://www.courts.state.hi.us/",
+      displayLink: "www.courts.state.hi.us",
+      snippet:
+        "The Judiciary is one of three branches of state government in Hawai`i. The other two are the executive and legislative branches.",
+      htmlSnippet:
+        "The Judiciary is one of three branches of state government in Hawai`i. The other two are the executive and legislative branches.",
+      cacheId: "SchY-JoA1uQJ",
+      formattedUrl: "https://www.courts.state.hi.us/",
+      htmlFormattedUrl: "https://www.courts.state.<b>hi</b>.us/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSdK_n6nErd6OoQ7v9r1Wgj5r_agDakcagQted7ewq26hHLsZYqYZWz9SLx",
+            width: "225",
+            height: "225",
+          },
+        ],
+        metatags: [
+          {
+            "og:image":
+              "https://www.courts.state.hi.us/wp-content/themes/hic_state_template_parent/images/og-image.jpg",
+            "twitter:card": "summary",
+            "og:type": "website",
+            viewport: "width=device-width, initial-scale=1",
+            "og:title": "Judiciary",
+            "og:url": "https://www.courts.state.hi.us",
+            "og:description": "Ka ʻOihana Hoʻokolokolo",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://www.courts.state.hi.us/wp-content/themes/hic_state_template_parent/images/og-image.jpg",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Hawaii County, HI | Home",
+      htmlTitle: "<b>Hawaii</b> County, <b>HI</b> | Home",
+      link: "https://www.hawaiicounty.gov/",
+      displayLink: "www.hawaiicounty.gov",
+      snippet:
+        "STAY INFORMED: HAWAII COUNTY CIVIL DEFENSE AGENCY INFORMATION AND LATEST UPDATES https://www.hawaiicounty.gov/civil-defense. Skip to Main Content ...",
+      htmlSnippet:
+        "STAY INFORMED: <b>HAWAII</b> COUNTY CIVIL DEFENSE AGENCY INFORMATION AND LATEST UPDATES https://www.hawaiicounty.gov/civil-defense. Skip to Main Content&nbsp;...",
+      cacheId: "614idEw7O8oJ",
+      formattedUrl: "https://www.hawaiicounty.gov/",
+      htmlFormattedUrl: "https://www.<b>hawaii</b>county.gov/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTwyK2husUlO6TF4bsP2Jo-sW0i3BQW1u_AnpkybZpNAWJQntyQ6bfdGek",
+            width: "275",
+            height: "183",
+          },
+        ],
+        metatags: [
+          {
+            viewport: "width=device-width",
+            "og:title": "Hawaii County, HI | Home",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://www.hawaiicounty.gov/home/showpublishedimage/1513/637032001724530000",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "What Hi-Fi? | Tech news and reviews",
+      htmlTitle: "What <b>Hi</b>-Fi? | Tech news and reviews",
+      link: "https://www.whathifi.com/us",
+      displayLink: "www.whathifi.com",
+      snippet:
+        "Latest hi-fi, home cinema and technology reviews, products, news, advice, videos and more, from the world's no.1 technology buyer's guide - What Hi-Fi?",
+      htmlSnippet:
+        "Latest <b>hi</b>-fi, home cinema and technology reviews, products, news, advice, videos and more, from the world&#39;s no.1 technology buyer&#39;s guide - What <b>Hi</b>-Fi?",
+      cacheId: "CVfIQEFjNw4J",
+      formattedUrl: "https://www.whathifi.com/us",
+      htmlFormattedUrl: "https://www.whathifi.com/us",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOdVOuO3TGcHxg7g8T5sDpjaCxweaCJ8UgxXBUzY0T9SVK9jyXs_hIA1w",
+            width: "212",
+            height: "238",
+          },
+        ],
+        metatags: [
+          {
+            "msapplication-tilecolor": "#000000",
+            "og:type": "article",
+            "twitter:card": "summary_large_image",
+            "theme-color": "#e02928",
+            "og:site_name": "whathifi",
+            "taboola:url": "https://www.whathifi.com/us",
+            "msvalidate.01": "CC462E6DF91492086145CA5EBDCB615E",
+            "msapplication-tileimage":
+              "https://vanilla.futurecdn.net/whathifi/571074/apple-touch-icon.png",
+            "twitter:creator": "@whathifi",
+            "article:publisher": "https://www.facebook.com/whathifi.com",
+            "fb:app_id": "235441786508054",
+            "twitter:site": "@whathifi",
+            viewport: "width=device-width,minimum-scale=1,initial-scale=1",
+            "og:url": "https://www.whathifi.com/us",
+            "botify-site-verification": "GbCOSB6kPpJcW9VsCADqNRe7MQXoTg69",
+            "twitter:account_id": "15560223",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://mos.fie.futurecdn.net/9i1oxqx9t6fdt1b1-1645536596015-250-80.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Hillenbrand Inc.",
+      htmlTitle: "<b>Hillenbrand Inc</b>.",
+      link: "https://www.hillenbrand.com/",
+      displayLink: "www.hillenbrand.com",
+      snippet:
+        "Hillenbrand is a global diversified industrial company with multiple market-leading brands that serve a wide variety of industries across the globe.",
+      htmlSnippet:
+        "Hillenbrand is a global diversified industrial company with multiple market-leading brands that serve a wide variety of industries across the globe.",
+      cacheId: "CmXJRwCMw7IJ",
+      formattedUrl: "https://www.hillenbrand.com/",
+      htmlFormattedUrl: "https://www.hillenbrand.com/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTZly5VxGDrHIG_znyLSvU62oLoNiALIWOY4qya7W4ktbtUdIc9g2YzbQN3",
+            width: "180",
+            height: "180",
+          },
+        ],
+        metatags: [
+          {
+            "og:image":
+              "https://cdn.zephyrcms.com/9cf9cdb6-4096-4f70-99e2-91254db2021e/-/progressive/yes/fb-image.jpg",
+            "og:type": "website",
+            viewport: "width=device-width, initial-scale=1.0",
+            "og:title": "Hillenbrand",
+            "og:description":
+              "Hillenbrand is a global diversified industrial company with multiple market-leading brands that serve a wide variety of industries across the globe. Hillenbrand’s portfolio is comprised of two business segments: the Process Equipment Group and Batesville",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://cdn.zephyrcms.com/9cf9cdb6-4096-4f70-99e2-91254db2021e/-/progressive/yes/fb-image.jpg",
+          },
+        ],
+      },
+    },
+  ],
+};
--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import SearchHeader from "../components/SearchHeader";
+import Response from "../Response";
 
-export default function search() {
+export default function search({ results }) {
+  console.log(results);
   return (
     <div>
       <Head>
@@ -13,4 +15,23 @@ export default function search() {
       {/* Search Results Component */}
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const testData = true; // set to true when testing (so we don't keep making requests)
+  const data = testData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${
+          process.env.API_KEY
+        }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
+          context.query.searchType && "&searchType=image"
+        }`
+      ).then((response) => response.json());
+
+  return {
+    props: {
+      results: data,
+    },
+  };
 }
