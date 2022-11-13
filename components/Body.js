3b772686d9cf953ff25963188f3d2ab57c021b7c
kevin <kevinzy17@gmail.com>
2022-11-12 18:23:42 -0800

    added searching functionality and useRef for searching

--- components/Body.js
+++ components/Body.js
@@ -1,8 +1,26 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { useRef } from "react";
+
 import { SearchIcon } from "@heroicons/react/solid";
 import { MicrophoneIcon } from "@heroicons/react/solid";
 
 function Body() {
+  const router = useRouter();
+
+  const searchInputRef = useRef(null);
+
+  const search = (e) => {
+    e.preventDefault();
+    const searchQuery = searchInputRef.current.value;
+
+    if (!searchQuery.trim()) {
+      return;
+    } else {
+      router.push(`/search?term=${searchQuery.trim()}`);
+    }
+  };
+
   return (
     <form className="flex flex-col items-center mt-40">
       <Image
@@ -17,11 +35,17 @@ function Body() {
         focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl"
       >
         <SearchIcon className="h-5 text-gray-500 mr-3" />
-        <input type="text" className="flex-grow focus:outline-none" />
+        <input
+          ref={searchInputRef}
+          type="text"
+          className="flex-grow focus:outline-none"
+        />
         <MicrophoneIcon className="h-5 ml-3" />
       </div>
       <div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
-        <button className="main-search-btn">Google Search</button>
+        <button onClick={search} className="main-search-btn">
+          Google Search
+        </button>
         <button className="main-search-btn">I&apos;m Feeling Lucky</button>
       </div>
     </form>
